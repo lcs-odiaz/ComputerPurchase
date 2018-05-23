9ce18c8372e9263691423816c7d9f5f255c292e5
Diaz Octavio <odiaz@lcs-mba3082.lcs.on.ca>
2018-05-22 21:37:05 -0400

    learning goal where use guard let to make sure not nil to protect form run time errors

--- ComputerPurchase/main.swift
+++ ComputerPurchase/main.swift
@@ -9,6 +9,25 @@ import Foundation
 // Global variable, tracks how many computers we are going to collect specs for
 var countOfExpectedSpecsThatWillBeProvided = 3
 
+while 1 == 1{
+    print("How many specs will be provided?")
+    
+    guard let givenInput = readLine() else{
+        continue
+    }
+    
+    guard let givenInteger = Int(givenInput) else{
+        continue
+    }
+    
+    if givenInteger < 1 || givenInteger > 10{
+        continue
+    }
+    
+    countOfExpectedSpecsThatWillBeProvided = givenInteger
+    break
+}
+
 // Write a loop to actually determine how many computer specs the user will provide
 // e.g.: write the rest of the INPUT section
 
@@ -29,25 +48,24 @@ for counter in 1...countOfExpectedSpecsThatWillBeProvided {
         // If someone enters nil input, just skip to the next line
         continue
     }
-    
     // What was provided?
-    print("The given input was: \(givenInput)")
+//    print("The given input was: \(givenInput)")
     
     // NOTE:
     //
     // Some example code that may be useful
-    let exampleInput = "SuperFastComputer 1000 50 75"
-    print("The example input is: \(exampleInput)")
-    let exampleInputPieces = exampleInput.split(separator: " ")
-    let computerName = exampleInputPieces[0]
-    let computerRAM = exampleInputPieces[1]
-    let computerCPU = exampleInputPieces[2]
-    let computerDiskSpace = exampleInputPieces[3]
-    print("Computer name is: \(computerName)")
-    print("Computer RAM amount is: \(computerRAM)")
-    print("Computer CPU speed is: \(computerCPU)")
-    print("Computer disk space is: \(computerDiskSpace)")
+//    let exampleInput = "SuperFastComputer 2 50 75"
+//    print("The example input is: \(exampleInput)")
+    let givenInputPieces = givenInput.split(separator: " ")
+    let computerName = givenInputPieces[0]
+    let computerRAM = Int(givenInputPieces[1])
+    let computerCPU = Int(givenInputPieces[2])
+    let computerDiskSpace = Int(givenInputPieces[3])
+//    print("Computer name is: \(computerName)")
+//    print("Computer RAM amount is: \(computerRAM)")
+//    print("Computer CPU speed is: \(computerCPU)")
+//    print("Computer disk space is: \(computerDiskSpace)")
     
     // Implement the rest of your logic here...
-    
+    let totalAddittionOfParts = 2 * computerRAM + 3 * computerCPU + computerDiskSpace
 }
